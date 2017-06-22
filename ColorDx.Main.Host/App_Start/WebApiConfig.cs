af66575e9e98e5db4216381b67714d382322a167
g.kisonas@adform.com <g.kisonas@adform.com>
2017-06-22 19:31:37 +0300

    WebApi + forms

--- ColorDx.Main.Host/App_Start/WebApiConfig.cs
+++ ColorDx.Main.Host/App_Start/WebApiConfig.cs
@@ -1,24 +1,29 @@
-﻿using System.Net.Http.Headers;
+﻿using Owin;
+using System.Net.Http.Headers;
 using System.Web.Http;
 
 namespace ColorDx.Main.Host
 {
-    public static class WebApiConfig
+    public class Startup
     {
-        public static void Register(HttpConfiguration config)
+        // This code configures Web API. The Startup class is specified as a type
+        // parameter in the WebApp.Start method.
+        public void Configuration(IAppBuilder appBuilder)
         {
-            // Web API configuration and services
+            HttpConfiguration config = new HttpConfiguration();
+
+            // Force JSON even if HTML requested - for easier debuging, we don't need HTML at all
             config.Formatters.JsonFormatter.SupportedMediaTypes
                 .Add(new MediaTypeHeaderValue("text/html"));
 
-            // Web API routes
-            config.MapHttpAttributeRoutes();
-
+            // Configure Web API for self-host. 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
                 routeTemplate: "{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+
+            appBuilder.UseWebApi(config);
         }
     }
 }
---
+++ ColorDx.Main.Host/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Owin.Hosting;
+using System.Net.Http;
+using System.Windows.Forms;
+
+namespace ColorDx.Main.Host
+{
+    class Program
+    {
+        [System.STAThread]
+        static void Main()
+        {
+            string baseAddress = "http://localhost:8000/";
+
+            using (WebApp.Start<Startup>(url: baseAddress))
+            {
+                Application.EnableVisualStyles();
+                Application.Run(new Form());
+            }
+        }
+    }
+}
