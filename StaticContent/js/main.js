a80e952a8b6b197abe69d1672f32ecf08ad584ce
abraxas93 <savchyk.a@gmail.com>
2017-06-29 21:03:51 +0300

    my initial commit

---
+++ StaticContent/js/main.js
@@ -0,0 +1,35 @@
+
+
+function renderBottomMenu(menu) {
+    var container = $('#main-nav .navigation');
+    
+    menu.forEach(function(item) {
+        var menuEl = $('<a/>', {
+            href: item.url,
+            id: item.id,
+            html: item.title,
+            click: item.onClick
+        });
+        $(menuEl).appendTo(container).wrap('<li></li>');
+        if(item.active) menuEl.addClass('active');
+    });
+}
+var menu = [{
+        id: 12,
+        title: "Keyboard",
+        url: '#',
+        onClick: function () {
+            console.log('clicked item: ' + this.id);
+        },
+        active: false
+    },
+    {
+        id: 24,
+        title: "Exit",
+        url: '#',
+        onClick: function () {
+            console.log('clicket item: ' + this.id);
+        },
+        active: true
+    }];
+
