908a7d96bcc30bd10f048d7d4551840f36fe0de9
abraxas93 <savchyk.a@gmail.com>
2017-07-06 18:21:25 +0300

    added new screen

--- StaticContent/js/main.js
+++ StaticContent/js/main.js
@@ -1,39 +1,31 @@
+var app = (function () {
+
+    function renderBottomMenu(menu) {
+        var container = $('#main-nav .navigation');
+        var marginRight = 37;
+
+        var pixels = -((menu.length - 1) * marginRight);
+
+        menu.forEach(function (item, i) {
+            var menuEl = $('<a/>', {
+                href: item.url,
+                id: item.id,
+                html: item.title,
+                click: item.onClick
+            });
+            $(menuEl).appendTo(container).wrap('<li class="' + item.class + '"></li>');
+            if (item.active) menuEl.parent().addClass('active');
+            if (i !== menu.length - 1) menuEl.parent().css('right', pixels);
+            pixels += marginRight;
+        });
+    }
+
+    return {
+        renderBotMenu: renderBottomMenu
+    }
+
+})();
 
 
-function renderBottomMenu(menu) {
-    var container = $('#main-nav .navigation');
-    
-    var pixels = -((menu.length - 1) * 37); 
-    
-    menu.forEach(function(item, i) {        
-        var menuEl = $('<a/>', {
-            href: item.url,
-            id: item.id,
-            html: item.title,
-            click: item.onClick
-        });              
-        $(menuEl).appendTo(container).wrap('<li class="'+ item.class +'"></li>');
-        if(item.active) menuEl.parent().addClass('active');
-        if(i !== menu.length - 1) menuEl.parent().css('right', pixels);
-        pixels += 37;
-    });
-}
-var menu = [{
-        id: 12,
-        title: "Keyboard",
-        url: '#',
-        onClick: function () {
-            console.log('clicked item: ' + this.id);
-        },
-        active: false
-    },
-    {
-        id: 24,
-        title: "Exit",
-        url: '#',
-        onClick: function () {
-            console.log('clicket item: ' + this.id);
-        },
-        active: true
-    }];
+
 
